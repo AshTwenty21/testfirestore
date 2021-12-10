5ffa9c4d4a65f80d1042563a5bc17641a84db8cf
rem-r <changivy.rems@gmail.com>
2021-12-10 22:46:12 +0100

    Assemblage des nouvelles activity + BbtFireSTORE

--- app/src/main/java/com/example/testfirestore/Question2.java
+++ app/src/main/java/com/example/testfirestore/Question2.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Question2 extends AppCompatActivity {
@@ -12,6 +14,14 @@ public class Question2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
+
+        Button button = (Button)  findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitybbt();
+            }
+        });
 
         Intent intent = getIntent();
         String texte = intent.getStringExtra(Question1.EXTRA_TEXT);
@@ -22,5 +32,10 @@ public class Question2 extends AppCompatActivity {
 
         textView1.setText(texte);
         textView2.setText(""+number);
+    }
+
+    void openActivitybbt() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
